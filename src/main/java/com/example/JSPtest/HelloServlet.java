d27e961408109d0906b690281055c9a603848524
Yan Agapov <Petrovic6776@gmail.com>
2021-06-06 17:35:52 +0300

    Initial commit

---
+++ src/main/java/com/example/JSPtest/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.example.JSPtest;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/")
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>HelloWorld</h1>");
+        out.println("</body></html>");
+    }
+
+}
